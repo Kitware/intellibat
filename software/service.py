d699f80c9dacde7c3920a0208107b3e5069b084d
Jason Parham <jason.parham@kitware.com>
2026-08-18 15:16:38 -0700

    Updates

--- software/service.py
+++ software/service.py
@@ -640,18 +640,13 @@ def chunk_triggers(data):
     peak_idx = np.argmax(magnitude)
     peak_freq = freqs[peak_idx]
 
-    print(magnitude)
-    print(peak_idx)
-    print(peak_freq)
-
-    peak_idxs = np.argsort(magnitude)[-5:]
+    peak_cutoff = int(len(magnitude) * 0.1)
+    print(peak_cutoff)
+    peak_idxs = np.argsort(magnitude)[-peak_cutoff:]
     peak_freqs = freqs[peak_idxs]
-    print(peak_idxs)
-    print(peak_freqs)
 
     trigger_freq = config_manager.config.minimum_trigger_frequency * 1000
     flag = np.any(peak_freqs > trigger_freq)
-    print(flag)
 
     return flag
 
